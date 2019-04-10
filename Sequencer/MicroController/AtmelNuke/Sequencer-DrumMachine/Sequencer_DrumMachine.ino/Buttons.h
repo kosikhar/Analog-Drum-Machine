a105ff50dfebb4e17d55732b9faf7b6d6c3770a0
aschroede <aschroed@ualberta.ca>
2019-04-10 00:05:47 -0600

    Create Buttons.h

---
+++ Sequencer/MicroController/AtmelNuke/Sequencer-DrumMachine/Sequencer_DrumMachine.ino/Buttons.h
@@ -0,0 +1,28 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 2019-04-09 11:27:18 PM
+ *  Author: b59mi
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+//Read button registers
+	// 1. Get data from the shift registers. 
+	// 2. Sort data
+	// 3. Load data into SeqIO
+	
+	seqIO.inputBytes.inputBuffer=InputSR.read()
+	
+
+//Light LEDS
+	// 1. Read data from SeqIO
+	// 2. Write data to leds
+	
+
+
+
+
+#endif /* INCFILE1_H_ */
